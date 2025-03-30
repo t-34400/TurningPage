5397ac2cc86925a416da16914c357a5841104867
t-34400 <34400mt@gmail.com>
2025-03-31 00:28:41 +0900

    Add new texture assets and implement MaterialSettingManager and PinchPoint scripts

---
+++ Sample/Scripts/MaterialSettingManager.cs
@@ -0,0 +1,34 @@
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TurningPage.Sample
+{
+    public class MaterialSettingManager : MonoBehaviour
+    {
+        [SerializeField] private TurningPageSimulation turningPageSimulation = default!;
+        [SerializeField] private List<FloatMaterialSettings> floatMaterialSettings = default!;
+
+        private void Start()
+        {
+            var frontMaterial = turningPageSimulation.FrontMaterial;
+            var backMaterial = turningPageSimulation.BackMaterial;
+
+            foreach (var settings in floatMaterialSettings)
+            {
+                frontMaterial.SetFloat(settings.propertyName, settings.frontMaterialPropertyValue);
+                backMaterial.SetFloat(settings.propertyName, settings.backMaterialPropertyValue);
+            }
+        }
+
+        [Serializable]
+        class FloatMaterialSettings
+        {
+            public string propertyName = "";
+            public float frontMaterialPropertyValue = 0;
+            public float backMaterialPropertyValue = 0;
+        }
+    }
+}
